74c2e392aeb4d31d7976badd102f9aa3d51b284a
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-18 10:06:06 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  roots: ['src'],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+};
