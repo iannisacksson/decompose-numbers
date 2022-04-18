0006aeb92fc88652b59a911f86a9dcc567154de7
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-18 15:28:03 -0300

    refactor: remove dependency

--- src/utils/FindDivisorsNumber.ts
+++ src/utils/FindDivisorsNumber.ts
@@ -1,17 +1,13 @@
-import { findPrimeDivisors } from './FindPrimeDivisors';
+export const findDivisorsNumber = (primeNumbers: number[]): number[] => {
+  const numberOneIndex = primeNumbers.findIndex(number => number === 1);
 
-export const findDivisorsNumber = (value: number): number[] => {
-  const primeDivisors = findPrimeDivisors(value);
-
-  const numberOneIndex = primeDivisors.findIndex(number => number === 1);
-
-  primeDivisors.splice(numberOneIndex, 1);
+  primeNumbers.splice(numberOneIndex, 1);
 
   const divisors = [1];
   let indexWhile = 0;
 
-  while (indexWhile < primeDivisors.length) {
-    const primeDivisor = primeDivisors[indexWhile];
+  while (indexWhile < primeNumbers.length) {
+    const primeDivisor = primeNumbers[indexWhile];
 
     divisors.forEach(divisor => {
       const newDivisor = primeDivisor * divisor;
@@ -24,5 +20,3 @@ export const findDivisorsNumber = (value: number): number[] => {
 
   return divisors.sort((divisorA, divisorB) => divisorA - divisorB);
 };
-
-findDivisorsNumber(45);
