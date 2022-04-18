85a14ecb3b85bff01cdbab39cd8f82789e45273f
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-18 10:27:11 -0300

    chore: add pluging of eslint

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,11 @@ export default {
   roots: ['src'],
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
+    '.+\\.ts$': 'ts-jest',
+  },
 };
