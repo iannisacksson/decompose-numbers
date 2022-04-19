cd0e431104773f2c237cda75eb5b674e3dd4b748
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-19 09:45:26 -0300

    refactor: transform method in class

--- src/utils/NumberDecomposer/implementations/FindDivisorsNumber.ts
+++ src/utils/NumberDecomposer/implementations/FindDivisorsNumber.ts
@@ -1,6 +1,9 @@
 import { IFindDivisorsOfNumber } from '../protocols/IFindDivisorsOfNumber';
+import { IFindPrimeDivisors } from '../protocols/IFindPrimeDivisors';
 
-export class NumberDecomposer implements IFindDivisorsOfNumber {
+export class NumberDecomposer
+  implements IFindDivisorsOfNumber, IFindPrimeDivisors
+{
   public findDivisorsOfNumber(primeNumbers: number[]): number[] {
     const divisors = [1];
     let indexWhile = 0;
@@ -22,5 +25,32 @@ export class NumberDecomposer implements IFindDivisorsOfNumber {
     );
 
     return sortedDivisors;
+  }
+
+  public findPrimeDivisors(value: number): number[] {
+    const primeDivisors: number[] = [];
+
+    const index = 1;
+    let numberDivider = 2;
+    let result = value;
+
+    while (result > 1) {
+      const newResult = result / numberDivider;
+
+      if (Number.isInteger(newResult)) {
+        primeDivisors.push(numberDivider);
+        result = newResult;
+      } else {
+        numberDivider += index;
+      }
+    }
+
+    primeDivisors.push(1);
+
+    const sortedPrimeDivisors = primeDivisors.sort(
+      (divisorA, divisorB) => divisorA - divisorB,
+    );
+
+    return sortedPrimeDivisors;
   }
 }
