86a2cb8841b13a7409246653b6e9473450ced1fd
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-19 10:22:05 -0300

    test: ensure FindDivisorsOfNumberUseCase call FindDivisorsOfNumber with correct values

--- src/data/useCases/FindDivisorsOfNumberUseCase.spec.ts
+++ src/data/useCases/FindDivisorsOfNumberUseCase.spec.ts
@@ -1,44 +1,72 @@
+import { IFindDivisorsOfNumber } from '../../utils/NumberDecomposer/protocols/IFindDivisorsOfNumber';
 import { IFindPrimeDivisors } from '../../utils/NumberDecomposer/protocols/IFindPrimeDivisors';
 import { FindDivisorsOfNumberUseCase } from './FindDivisorsOfNumberUseCase';
 
 interface ISut {
-  numberDecomposerStub: IFindPrimeDivisors;
+  findPrimeDivisorsStub: IFindPrimeDivisors;
+  findDivisorsOfNumberStub: IFindDivisorsOfNumber;
   findDivisorsOfNumberUseCase: FindDivisorsOfNumberUseCase;
 }
 
-const makeNumberDecomposer = () => {
-  class NumberDecomposerStub implements IFindPrimeDivisors {
+const makeFindPrimeDivisors = () => {
+  class FindPrimeDivisors implements IFindPrimeDivisors {
     public findPrimeDivisors(): number[] {
-      return [1, 3, 5];
+      return [1, 3, 3, 5];
     }
   }
 
-  return new NumberDecomposerStub();
+  return new FindPrimeDivisors();
+};
+
+const makeFindDivisorsOfNumber = () => {
+  class FindDivisorsOfNumber implements IFindDivisorsOfNumber {
+    public findDivisorsOfNumber(): number[] {
+      return [1, 3, 5, 9, 15, 45];
+    }
+  }
+
+  return new FindDivisorsOfNumber();
 };
 
 const makeSut = (): ISut => {
-  const numberDecomposerStub = makeNumberDecomposer();
+  const findPrimeDivisorsStub = makeFindPrimeDivisors();
+  const findDivisorsOfNumberStub = makeFindDivisorsOfNumber();
   const findDivisorsOfNumberUseCase = new FindDivisorsOfNumberUseCase(
-    numberDecomposerStub,
+    findPrimeDivisorsStub,
+    findDivisorsOfNumberStub,
   );
 
   return {
-    numberDecomposerStub,
+    findPrimeDivisorsStub,
     findDivisorsOfNumberUseCase,
+    findDivisorsOfNumberStub,
   };
 };
 
 describe('FindDivisorsOfNumber UseCase', () => {
   it('Should call FindPrimeDivisors with correct values', () => {
-    const { findDivisorsOfNumberUseCase, numberDecomposerStub } = makeSut();
+    const { findDivisorsOfNumberUseCase, findPrimeDivisorsStub } = makeSut();
 
     const spyFindPrimeDivisors = jest.spyOn(
-      numberDecomposerStub,
+      findPrimeDivisorsStub,
       'findPrimeDivisors',
     );
 
     findDivisorsOfNumberUseCase.execute(45);
 
     expect(spyFindPrimeDivisors).toHaveBeenCalledWith(45);
+  });
+
+  it('Should call FindDivisorsOfNumber with correct values', () => {
+    const { findDivisorsOfNumberUseCase, findDivisorsOfNumberStub } = makeSut();
+
+    const spyFindDivisorsOfNumber = jest.spyOn(
+      findDivisorsOfNumberStub,
+      'findDivisorsOfNumber',
+    );
+
+    findDivisorsOfNumberUseCase.execute(45);
+
+    expect(spyFindDivisorsOfNumber).toHaveBeenCalledWith([1, 3, 3, 5]);
   });
 });
