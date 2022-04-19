db3f9da0915f64daa7e2fd3163f7083e87b6a314
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-19 10:21:58 -0300

    feat: ensure FindDivisorsOfNumberUseCase call FindDivisorsOfNumber with correct values

--- src/data/useCases/FindDivisorsOfNumberUseCase.ts
+++ src/data/useCases/FindDivisorsOfNumberUseCase.ts
@@ -1,12 +1,18 @@
 import { IPrimeDivisors } from '../../domain/models/IPrimeDivisors';
 import { IFindPrimeDivisorsUseCase } from '../../domain/useCases/IFindPrimeDivisorsUseCase';
+import { IFindDivisorsOfNumber } from '../../utils/NumberDecomposer/protocols/IFindDivisorsOfNumber';
 import { IFindPrimeDivisors } from '../../utils/NumberDecomposer/protocols/IFindPrimeDivisors';
 
 export class FindDivisorsOfNumberUseCase implements IFindPrimeDivisorsUseCase {
-  constructor(private findPrimeDivisors: IFindPrimeDivisors) {}
+  constructor(
+    private findPrimeDivisors: IFindPrimeDivisors,
+    private findDivisorsOfNumber: IFindDivisorsOfNumber,
+  ) {}
 
   public execute(value: number): IPrimeDivisors {
-    this.findPrimeDivisors.findPrimeDivisors(value);
+    const primeDivisors = this.findPrimeDivisors.findPrimeDivisors(value);
+
+    this.findDivisorsOfNumber.findDivisorsOfNumber(primeDivisors);
 
     return null;
   }
