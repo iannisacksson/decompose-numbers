11fb30bf7bbb3e0f63aba7bc81a5bc655039ac0f
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-19 12:12:29 -0300

    chore: add script

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,7 @@
 import { app } from './config/app';
 
 app.listen(process.env.PORT || 3333, () =>
-  console.log(`Server running at http://localhost:${process.env.PORT || 3333}`),
+  console.log(
+    `🚀️ Server running at http://localhost:${process.env.PORT || 3333}`,
+  ),
 );
