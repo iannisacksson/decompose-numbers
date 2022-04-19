60d16e3f7f51f7959d38abfd3b323fdfd89c569a
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-19 11:54:15 -0300

    refactor: change name node_port to port

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,5 @@
 import { app } from './config/app';
 
-app.listen(process.env.NODE_PORT || 3333, () =>
-  console.log(
-    `Server running at http://localhost:${process.env.NODE_PORT || 3333}`,
-  ),
+app.listen(process.env.PORT || 3333, () =>
+  console.log(`Server running at http://localhost:${process.env.PORT || 3333}`),
 );
