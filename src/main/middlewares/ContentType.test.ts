99d2b468279f575685a2042f51065d7893d92680
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-19 10:54:54 -0300

    test: ensure requests return json as default content type

---
+++ src/main/middlewares/ContentType.test.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import request from 'supertest';
+import { app } from '../config/app';
+
+describe('Content Type Middleware', () => {
+  test('Should return default content type as json', async () => {
+    app.get('/test_content_type', (request: Request, response: Response) => {
+      response.send('');
+    });
+
+    await request(app).get('/test_content_type').expect('content-type', /json/);
+  });
+});
